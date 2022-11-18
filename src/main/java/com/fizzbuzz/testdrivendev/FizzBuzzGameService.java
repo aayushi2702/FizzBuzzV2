f53527b35ce616bf4f5e62dc4a1d2cf10fe0a593
aayushi2702 <48720540+aayushi2702@users.noreply.github.com>
2022-11-18 18:20:43 +0530

    feat: FizzBuzz game should return Fizz as an output when number 3 is passed as an output.

--- src/main/java/com/fizzbuzz/testdrivendev/FizzBuzzGameService.java
+++ src/main/java/com/fizzbuzz/testdrivendev/FizzBuzzGameService.java
@@ -2,7 +2,8 @@ package com.fizzbuzz.testdrivendev;
 
 public class FizzBuzzGameService {
 	public static String getFizzBuzzGameOutput(int number) {
-
+		if (number == 3)
+			return "Fizz";
 		return String.valueOf(number);
 	}
 }
